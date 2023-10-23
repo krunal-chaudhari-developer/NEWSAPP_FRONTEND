452c61fff0581813c69bcf03f5dbb68a6dd21a7b
krunal-chaudhari-developer <chaudharikrunal825@gmail.com>
2023-10-23 22:33:46 +0530

    Make a news landing page, add category and search bar and pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NewsPage from "./Components/NewsPage";
 
 const App = () => {
-  return <div>App App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<NewsPage category="general" />} />
+          <Route path="/business" element={<NewsPage category="business" />} />
+          <Route
+            path="/entertainment"
+            element={<NewsPage category="entertainment" />}
+          />
+          <Route path="/health" element={<NewsPage category="health" />} />
+          <Route path="/science" element={<NewsPage category="science" />} />
+          <Route path="/sports" element={<NewsPage category="sports" />} />
+          <Route
+            path="/technology"
+            element={<NewsPage category="technology" />}
+          />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BsSearch } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { category } from "./data";
+
+const Navbar = ({ search, setSearch, handleSearch }) => {
+  return (
+    <>
+      <div className="bg-slate-200 w-full py-3 px-10 md:flex justify-between">
+        <div className="">
+          <h1 className="font-bold text-xl text-slate-700">NewMonkey</h1>
+        </div>
+
+        <div className="grid grid-cols-2 my-2 sm:my-0 lg:flex lg:space-x-3">
+          {category.map((e) => (
+            <Link to={`/${e.name}`} key={e.id} className="">
+              <h1 className="capitalize">{e.name}</h1>
+            </Link>
+          ))}
+        </div>
+
+        <div className="lg:flex justify-center ">
+          <h1 className="px-2 font-bold hidden md:block">Search :</h1>
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="lg:w-60 rounded-l-md outline-none py-0.5 px-1 text-sm "
+          />
+          <button
+            onClick={handleSearch}
+            className="py-1 px-2 md:px-2 rounded-r-md bg-black"
+          >
+            <BsSearch className="text-white" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
