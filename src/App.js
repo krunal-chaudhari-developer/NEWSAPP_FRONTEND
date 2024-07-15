16d4431b2c2ab43c9278606bc6a60a9d6e97b1b3
krunal-chaudhari-developer <chaudharikrunal825@gmail.com>
2024-07-15 23:03:32 +0530

    API calling Complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import NewsPage from "./Components/NewsPage";
-import Starred from "./Components/Starred";
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<NewsPage category="general" />} />
-          <Route path="/business" element={<NewsPage category="business" />} />
-          <Route
-            path="/entertainment"
-            element={<NewsPage category="entertainment" />}
-          />
-          <Route path="/health" element={<NewsPage category="health" />} />
-          <Route path="/science" element={<NewsPage category="science" />} />
-          <Route path="/sports" element={<NewsPage category="sports" />} />
-          <Route
-            path="/technology"
-            element={<NewsPage category="technology" />}
-          />
-          <Route path="/:starred" element={<Starred />} />
-        </Routes>
-      </Router>
+      <div className="">
+        <Navbar />
+      </div>
     </>
   );
 };
